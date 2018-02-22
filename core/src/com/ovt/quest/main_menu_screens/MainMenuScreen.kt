3b3492f43fc226cd48b6277b98834d04ae1fd851
Nikolay <kombarov55@gmail.com>
2018-02-22 21:03:30 +0300

    изменён размер кнопок в главном меню

--- core/src/com/ovt/quest/main_menu_screens/MainMenuScreen.kt
+++ core/src/com/ovt/quest/main_menu_screens/MainMenuScreen.kt
@@ -3,58 +3,41 @@ package com.ovt.quest.main_menu_screens
 
 import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.Screen
-import com.badlogic.gdx.audio.Music
 import com.badlogic.gdx.graphics.GL20
 import com.badlogic.gdx.scenes.scene2d.Stage
 import com.badlogic.gdx.scenes.scene2d.ui.Table
-import com.badlogic.gdx.utils.Timer
 import com.ovt.quest.commons.QuestGame
 import com.ovt.quest.quest.layout.QuestScreen
 
 /**
  * Created by kombarov_na on 21.12.2017.
  */
-
 class MainMenuScreen(internal var game: QuestGame) : Screen {
 
     private val stage = Stage()
 
     override fun show() {
+
+        val h = Gdx.graphics.height
+        val w = Gdx.graphics.width
+
         Gdx.input.inputProcessor = stage
 
         stage.addActor(game.background)
 
         val playButton = game.buttons.biggerButton("Играть", { game.screen = QuestScreen(game) })
 
-//        val minigames = game.buttons.biggerButton("Миниигры", { game.screen = MinigamesScreen(game) })
-
         val exitButton = game.buttons.biggerButton("Выход", { Gdx.app.exit() })
 
         val table = Table()
         table.setFillParent(true)
-        table.defaults().width(Gdx.graphics.width * 0.50f)
+        table.defaults().width(Gdx.graphics.width * 0.8f).pad(h * 0.008f)
 
         table.add(playButton)
         table.row()
-//        table.add(minigames)
-//        table.row()
         table.add(exitButton)
 
         stage.addActor(table)
-    }
-
-    private fun fadeOutMusic(music: Music) {
-        var volume: Float = music.volume * 100
-
-        Timer().scheduleTask(object : Timer.Task() {
-            override fun run() =
-                if (volume <= 0f) {
-                    this.cancel()
-                } else {
-                    volume -= 1
-                }
-
-        }, 0f, 10f)
     }
 
     override fun render(delta: Float) {
