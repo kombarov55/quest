3957a34a90c34f40b557a56b3f0ff89d286c9748
Nikolay <kombarov55@gmail.com>
2018-04-12 02:21:10 +0300

    added initial tests

--- core/src/com/ovt/test/ThreeInARow/MatrixTests.kt
+++ core/src/com/ovt/test/ThreeInARow/MatrixTests.kt
@@ -4,43 +4,61 @@ import com.ovt.quest.three_in_a_row.model.GroupFinder
 import com.ovt.quest.three_in_a_row.model.Item
 import com.ovt.quest.three_in_a_row.model.Item.Type.*
 import com.ovt.quest.three_in_a_row.model.Matrix
+import io.kotlintest.matchers.containsAll
+import io.kotlintest.matchers.should
+import io.kotlintest.matchers.shouldBe
+import io.kotlintest.specs.StringSpec
 import java.nio.file.Files
 import java.nio.file.Paths
 
 
-fun main(args: Array<String>) {
-    runTests()
+class findSimpleGroup: StringSpec() {
+    init {
+        "should find all groups in matrix" {
+            val m = loadMatrixFromFile("/Users/nikolay/IdeaProjects/quest/matrix-test-samples/test-find-simple-group")
+            val groups = GroupFinder.findGroups(m)
+            groups.first() should containsAll(m.get(0, 1), m.get(1,1), m.get(2, 1))
+        }
+    }
 }
 
-fun runTests() {
-    testFindingGroups()
+class findTwoGroups: StringSpec() {
+    init {
+        val m = loadMatrixFromFile("/Users/nikolay/IdeaProjects/quest/matrix-test-samples/test-two-groups")
+        val groups = GroupFinder.findGroups(m)
+        groups.size shouldBe 2
+        groups[0] should containsAll (m.get(1, 1), m.get(2, 1), m.get(3, 1), m.get(4, 1))
+        groups[1] should containsAll(m.get(1, 3), m.get(2, 3), m.get(3, 3))
+    }
 }
+
+class findAngleGroups: StringSpec() {
+    init {
+        var m = loadMatrixFromFile("/Users/nikolay/IdeaProjects/quest/matrix-test-samples/test-left-down-angle")
+        var groups = GroupFinder.findGroups(m)
+//        groups.first() should containsAll(m.get(3, 1), m.get(4, 1), m.get(5, 1), m.get(3, 2), m.get(3, 3))
+
+        m = loadMatrixFromFile("/Users/nikolay/IdeaProjects/quest/matrix-test-samples/test-left-up-angle")
+        groups = GroupFinder.findGroups(m)
+
+        m = loadMatrixFromFile("/Users/nikolay/IdeaProjects/quest/matrix-test-samples/test-right-up-angle")
+        groups = GroupFinder.findGroups(m)
+
+        m = loadMatrixFromFile("/Users/nikolay/IdeaProjects/quest/matrix-test-samples/test-right-down-angle")
+        groups = GroupFinder.findGroups(m)
+    }
+}
+
+class findCrossGroup: StringSpec() {
+    init {
+
+    }
+}
+
 
 fun testFindingGroups() {
     var m = loadMatrixFromFile("/Users/nikolay/IdeaProjects/quest/matrix-test-samples/test-group-finding-1")
     var groups = GroupFinder.findGroups(m)
-    if (groups.contains(listOf(m.get(0, 1), m.get(1,1), m.get(2, 1)))) {
-        println("first test: ok")
-    } else {
-        println("first test: fail")
-    }
-
-    m = loadMatrixFromFile("/Users/nikolay/IdeaProjects/quest/matrix-test-samples/test-group-finding-2")
-    groups = GroupFinder.findGroups(m)
-    var required = listOf(
-            listOf(m.get(1, 1), m.get(2, 1), m.get(3, 1), m.get(4, 1)),
-            listOf(m.get(1, 3), m.get(2, 3), m.get(3, 3))
-
-    )
-
-    if (required.all { groups.contains(it) }) {
-        println("second test: ok")
-    } else {
-        println("second test: fail")
-        m.print()
-        println("found groups: $groups")
-        println("required groups: $required")
-    }
 
     m = loadMatrixFromFile("/Users/nikolay/IdeaProjects/quest/matrix-test-samples/test-group-finding-2")
     groups = GroupFinder.findGroups(m)
