cc6ea3bf4d369314d151981865cd8ab8dd1ff4c5
na_kombarov <kombarov55@gmail.com>
2018-04-29 00:32:15 +0300

    added tilemap and rendering tilemap

--- core/src/com/ovt/quest/archery/ArcheryScreen.kt
+++ core/src/com/ovt/quest/archery/ArcheryScreen.kt
@@ -3,6 +3,9 @@ package com.ovt.quest.archery
 import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.Screen
 import com.badlogic.gdx.graphics.GL20
+import com.badlogic.gdx.graphics.OrthographicCamera
+import com.badlogic.gdx.maps.tiled.TmxMapLoader
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer
 import com.ovt.quest.QuestGame
 
 /**
@@ -10,14 +13,20 @@ import com.ovt.quest.QuestGame
  */
 class ArcheryScreen(private val game: QuestGame) : Screen {
 
+    val cam = OrthographicCamera()
+
+    private val tilemap = TmxMapLoader().load("maps/archery/archery.tmx")
+    private val tmxRenderer = OrthogonalTiledMapRenderer(tilemap, 1f / 16)
 
 
     override fun show() {
+        cam.setToOrtho(false, 10f, 16.6f)
     }
 
     override fun render(delta: Float) {
-        Gdx.gl.glClearColor(1f, 1f, 1f, 1f)
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
+        tmxRenderer.setView(cam)
+        tmxRenderer.render()
     }
 
     override fun hide() { }
